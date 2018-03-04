f47adb5df8e2744b4ba3e1618f4e7a600decb6e7
Alexander Curtiss <apccurtiss@gmail.com>
2018-03-03 20:36:54 -0700

    Made the context menu turn the clicked element red.

--- background.js
+++ background.js
@@ -2,7 +2,15 @@ chrome.contextMenus.create(
   {
     title: "Add to calendar",
     visible: true,
-    onclick: () => { alert("Feature not implemented.") }
+    onclick: addToCalendar
   },
-  () => {}
+  onCompletion
 );
+
+function addToCalendar(details, tab) {
+  chrome.tabs.sendMessage(tab.id, {"message": "add_to_calendar"});
+};
+
+function onCompletion() {
+  console.log("Successfully added context menu! Or unsuccessfully. I don't actually check which.")
+}
--- content.js
+++ content.js
@@ -1,1 +1,20 @@
-alert("asdf");
+lastMouseClick = undefined;
+
+document.addEventListener('pointerdown', (e) => {
+  if(e) {
+    if(e.target) {
+      lastMouseClick = e.target;
+    }
+  }
+});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.message === "add_to_calendar") {
+      if(lastMouseClick) {
+        lastMouseClick.style.background = "#F00";
+        console.log("User clicked on: ", lastMouseClick.tagName);
+      }
+    }
+  }
+);
